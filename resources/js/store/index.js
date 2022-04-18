28e2a80c3d64ab983684b7f1b65fdd2b8e91ec2a
Artem Kanarskyi <kanarskyiartem@gmail.com>
2022-04-18 11:52:56 +0300

    update login and register page

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,9 +1,11 @@
 import Vuex from 'vuex'
+import auth from "./modules/auth";
 
 import middleware from './modules/middleware';
 
 export default new Vuex.Store({
     modules: {
-        middleware
+        middleware,
+        auth
     }
 })
